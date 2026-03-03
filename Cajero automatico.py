a4f5de736715ac99b3eb8b2eaa1dcd7ed5df7e20
Keinerdmb <Keinermb01@gmail.com>
2026-03-03 00:21:13 -0500

    Cambios

--- Cajero automatico.py
+++ Cajero automatico.py
@@ -1,8 +1,8 @@
+import random 
 Salir = False
 Saldo = 0
 Retirar_dinero = 0
 Deposito = 0
-Clave = 123456
 Movimiento= []
 
 while not Salir:
@@ -10,14 +10,14 @@ while not Salir:
 #OPCION 1: CLAVES
  print("       ""SELECCIONE SU OPERACION"" \n ")
  print("GESTION DE RETIRO RAPIDO \n")
- print("1. CLAVES             2. CONSULTAS DE MOVIMIENTO \n")
+ print("1. CLAVE             2. CONSULTAS DE MOVIMIENTO \n")
  print("3. PAGOS              4. RETIRO\n")
  print("5. CONSULTAR          6. TRANSFERENCIA\n")
  print("7. DEPOSITAR\n") 
  opcion = input( "SELECCIONE UNA OPCION:")
 
  if opcion == "1":
-    print (f"Tus claves son: {Clave}\n")
+    print (f"Tu clave es: {random.randint(100000, 999999)}\n")
     while True:
 
      print ("1. Volver al menu     2. Salir \n")
